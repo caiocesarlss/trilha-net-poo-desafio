129cd754bb254bcc7d18cd1da2ac1a5efa4d485a
Caio César Leal <caioleal017@gmail.com>
2023-01-25 22:22:33 -0300

    Desafio concluído!

--- Program.cs
+++ Program.cs
@@ -3,12 +3,12 @@
 Console.WriteLine("Smartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "1234", modelo: "Nokia XR20", imei: "11111111", memoria: 64);
 nokia.Ligar();
-nokia.InstalarAplicativo("WhatsApp");
+nokia.InstalarAplicativo("TikTok");
 
 Console.WriteLine();
 
 Console.WriteLine("Smartphone iPhone:");
 Smartphone iphone = new Iphone(numero: "5678", modelo: "iPhone 14", imei: "22222222", memoria: 128);
 iphone.ReceberLigacao();
-iphone.InstalarAplicativo("Telegram");
+iphone.InstalarAplicativo("Twitter");
 
